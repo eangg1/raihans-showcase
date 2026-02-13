d2618bfb45be133ea05a6931a0bb1cb3d7eccdbe
rehan keselek motor <158054737+eangg1@users.noreply.github.com>
2026-02-13 20:56:45 +0700

    commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -17,45 +17,6 @@ export default function Home() {
             <p className="text-gray-500 dark:text-gray-400 mb-6 leading-relaxed">
               Saya adalah lulusan bisnis daring dan pemasaran yang memiliki pengalaman di bidang operasional, administrasi, manajemen inventaris, serta pelayanan pelanggan. terbiasa bekerja dalam tim, mengelola banyak tugas sekaligus, dan menjaga ketelitian dalam pekerjaan. memiliki kemampuan kepemimpinan, manajemen waktu, serta mampu beradaptasi di lingkungan kerja yang dinamis. terbuka untuk peluang kerja, freelance, dan kolaborasi proyek.
             </p>
-
-            {/* Buttons */}
-            <div className="flex flex-wrap gap-4 justify-center md:justify-start">
-              <a
-                href="/public/assets/CV-RAIHAN-DWI-SAPUTRA.pdf"
-                download
-                className="flex items-center gap-2 px-5 py-2 rounded-xl bg-black text-white dark:bg-white dark:text-black hover:opacity-90 transition"
-              >
-                <Download size={16} />
-                download cv
-              </a>
-
-              <a
-                href="/projects"
-                className="flex items-center gap-2 px-5 py-2 rounded-xl border border-gray-300 dark:border-gray-700 hover:bg-gray-100 dark:hover:bg-gray-800 transition"
-              >
-                <Folder size={16} />
-                view projects
-              </a>
-
-             <a
-              href="https://mail.google.com/mail/?view=cm&fs=1&to=raihands.j@gmail.com&su=Kontak%20dari%20Website&body=Halo%20saya%20ingin%20menghubungi%20anda"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center gap-2 px-5 py-2 rounded-xl border border-gray-300 dark:border-gray-700 hover:bg-gray-100 dark:hover:bg-gray-800 transition"
-            >
-              <Mail size={16} />
-              kontak saya
-            </a>
-            </div>
-          </div>
-
-          {/* RIGHT — Photo */}
-          <div className="flex justify-center md:justify-end">
-            <img
-              src="/assets/profile.jpg"
-              alt="profile"
-              className="w-60 h-60 md:w-72 md:h-72 rounded-full object-cover shadow-lg"
-            />
           </div>
         </div>
       </section>
